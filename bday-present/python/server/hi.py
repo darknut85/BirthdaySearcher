4ef8dd9e0ceb0f8715f399e1d0e79fe6e71b3dd5
marco <marco.haasjes22@gmail.com>
2023-05-22 10:53:42 +0200

    python running

--- bday-present/python/server/hi.py
+++ bday-present/python/server/hi.py
@@ -1,11 +1,15 @@
 from flask import Flask, request
 from flask_cors import CORS
 
+first = 0
+second = 0
+answer = 0
+
 app = Flask(__name__)
 CORS(app)
 
 @app.get('/sayHello')
 
-@app.route('/sayHello/hi')
+@app.route('/sayHello/Hi')
 def Hi():
     return "Hi"
