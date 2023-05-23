b3feb70a999d7a73a5e79bbe19e865f40e56c7b9
marco <marco.haasjes22@gmail.com>
2023-05-23 14:26:58 +0200

    changed query to separate parameter

--- bday-present/python/server/services/presentService.py
+++ bday-present/python/server/services/presentService.py
@@ -1,19 +1,12 @@
 from config import ConnectionString
 
-def AddPresents():
-    conn = ConnectionString.returnConn()
-    conn.autocommit = True
-    cursor = conn.cursor()
-    query = "INSERT INTO presents(ID, NAME, OWNER) VALUES(%s, %s, %s)"
-    recordsToInsert = (2, 'Flower', 'Isaac')
-    cursor.execute(query, recordsToInsert)
-    conn.commit()
-
 def GetPresents():
     conn = ConnectionString.returnConn()
     conn.autocommit = True
     cursor = conn.cursor()
-    cursor.execute('''SELECT * from presents''')
+
+    query = '''SELECT * from presents'''
+    cursor.execute(query)
 
     columns = cursor.description
     results = [{columns[index][0]:column for index, column in enumerate(value)} for value in cursor.fetchall()]
@@ -21,19 +14,37 @@ def GetPresents():
     conn.commit()
     return results
 
+def AddPresents():
+    conn = ConnectionString.returnConn()
+    conn.autocommit = True
+    cursor = conn.cursor()
+
+    query = "INSERT INTO presents(ID, NAME, OWNER) VALUES(%s, %s, %s)"
+    recordsToInsert = (2, 'Flower', 'Isaac')
+    cursor.execute(query, recordsToInsert)
+    conn.commit()
+
 def DeletePresents():
     conn = ConnectionString.returnConn()
     conn.autocommit = True
     cursor = conn.cursor()
-    cursor.execute('DELETE from presents WHERE id = 2')
+
+    recordsToInsert = ('2')
+    query = "DELETE from presents WHERE id = %s"
+    cursor.execute(query, recordsToInsert)
     conn.commit()
 
 def UpdatePresents():
     conn = ConnectionString.returnConn()
     conn.autocommit = True
     cursor = conn.cursor()
-    cursor.execute('UPDATE presents SET name = %s WHERE id = %s',('Car', 1))
+
+    recordsToInsert = ('Car', '2')
+    query = "UPDATE presents SET name = %s WHERE id = %s"
+    cursor.execute(query, recordsToInsert)
     conn.commit()
+
+    
 
 def getPresents():
     return GetPresents()
