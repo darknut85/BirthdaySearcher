f0248e16c0ff33d0d2be163f45d652b6d3c764e4
marco <marco.haasjes22@gmail.com>
2023-05-24 14:48:18 +0200

    Changed app page to home page

---
+++ bday-present/src/components/App/App.js
@@ -0,0 +1,8 @@
+import { Home } from "../Home/Home";
+
+function App() {
+  let a = true;
+  if(a)
+    return Home();
+}
+export default App;
---
+++ bday-present/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+export function Home() {
+    return (
+        <div className="App">
+        <header className="App-header">
+            <p>
+            Home Page
+            </p>
+        </header>
+        </div>
+    );
+}
