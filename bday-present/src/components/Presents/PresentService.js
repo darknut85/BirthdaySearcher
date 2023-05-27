f0b5c0788da6363dfdd6b2f587ac0cde36189919
marco <marco.haasjes22@gmail.com>
2023-05-27 15:07:11 +0200

    prevent data from showing twice

--- bday-present/src/components/Presents/PresentService.js
+++ bday-present/src/components/Presents/PresentService.js
@@ -6,24 +6,26 @@ var localhost = 'http://127.0.0.1:5000/presents/Get'
 export function Get(){
   document.body.style.backgroundColor = "red";
   const[divText] = useState('');
-  const a = <div id="idf">
-              {divText}
-            </div>
+
   fetch(localhost)
     .then(response => response.json()).then(data => 
       {
         var par = document.getElementById("idf");
+
         data.forEach(addData)
         function addData(item) 
         {
-          par.innerText += item.id;
-          par.innerText += item.name;
-          par.innerText += item.owner;
+          const present = new Present(item.id,item.name,item.owner);
+          par.innerText += present.id;
+          par.innerText += present.name;
+          par.innerText += present.owner;
         };
       }
     );
-
- 
+  const a = <div id="idf">
+              {divText}
+            </div>
+    
   return (a);
 }
 
--- bday-present/src/index.js
+++ bday-present/src/index.js
@@ -7,9 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 //display content depending on page
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
 
 reportWebVitals();
