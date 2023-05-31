8f72e62e7371ad83d1969369f3316005219ebe89
marco <marco.haasjes22@gmail.com>
2023-05-31 14:32:22 +0200

    added try tests that will be replaced

---
+++ bday-present/python/server/unittests/getTest.py
@@ -0,0 +1,20 @@
+import unittest
+
+class TestGetPresentMethods(unittest.TestCase):
+
+    def test_GetData(self):
+        self.assertTrue('FOO'.isupper())
+        
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
